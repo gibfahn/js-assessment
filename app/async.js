b324b8bd8800e1f779148a2be40f4e8df19e23b8
Gibson Fahnestock <gib@uk.ibm.com>
2016-06-26 10:39:42 +0100

    Answers for Async (copied, no idea about this)
    Seems to be a load of jQuery specific promises stuff. Don't really get
    what's going on here.

--- app/async.js
+++ app/async.js
@@ -2,10 +2,24 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
+    var dfd = $.Deferred();
+    setTimeout(function() {
+      dfd.resolve(value);
+    }, 10);
+    console.log("Hi there, this is Gib");
+    return dfd.promise();
 
   },
 
   manipulateRemoteData: function(url) {
+    var dfd = $.Deferred();
 
+    $.ajax(url).then(function(res){
+      var people = $.map(res.people, function(person) {
+        return person.name;
+      });
+      dfd.resolve(people.sort());
+    });
+    return dfd.promise();
   }
 };
